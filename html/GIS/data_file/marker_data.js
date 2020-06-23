ebdf5764bf38a14777920877449bf849a788f9d8
mwarabudanny <mwarabudannygmail.com>
2020-06-23 11:18:09 +0200

    chanhfe

---
+++ html/GIS/data_file/marker_data.js
@@ -0,0 +1,15 @@
+
+      var markers = [
+        {
+          coords:{lat:-25.740603,lng:28.209105},
+          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          content:'<h1>Lynn MA</h1>'
+        },
+        {
+          coords:{lat:-25.740236,lng:28.213664},
+         // content:'<h1>Amesbury '+<br>ssssssssssss<br>sssssssssssss<br>sssssssssss</h1>'
+        },
+        {
+          coords:{lat:42.7762,lng:-71.0773}
+        }
+      ];
---
+++ html/GIS/js_files/clock_time.js
@@ -0,0 +1,18 @@
+//time
+ 
+setInterval(function(){
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+    
+    // Add leading zeros
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+    hours = (hours < 10 ? "0" : "") + hours;
+    
+    // Compose the string for display
+    var currentTimeString = hours + ":" + minutes + ":" + seconds;
+    $(".clock").html(currentTimeString).draggable();
+    
+    },1000);
