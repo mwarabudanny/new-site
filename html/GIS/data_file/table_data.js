1a350e1dcf89477361717842b939af29ad10c1e9
mwarabudanny <mwarabudannygmail.com>
2020-06-24 21:09:31 +0200

    hgsjgllidd

--- html/GIS/data_file/table_data.js
+++ html/GIS/data_file/table_data.js
@@ -1,5 +1,8 @@
 
 $( document ).ready(function() {
+
+	var red_status='<img src="icons_maps/red.png" width="20" height="20">'
+	var green_status='<img src="icons_maps/green.png" width="20" height="20">'
     var html=`
 <div class="table-services">
 	<div style="width: 600px">
@@ -9,43 +12,32 @@ $( document ).ready(function() {
 		</colgroup>
 		<thead>
 		<tr>
-		<th>Header</th>
-		<th>status</th>
-		<th>TL name</th>
-		<th>TL number</th>
+		<th>Region</th>
+		<th>offLine TF</th>
+		<th>online TF</th>
+		<th>Tolat</th>
 		</tr>
 		</thead>
 		<tbody>
 			<tr>
-			<td>item</td>
-			<td>item</td>
-			<td>item</td>
-			<td>item</td>
+			<td>GAU</td>
+			<td><img src="icons_maps/red.png" width="20" height="20"> / 3 </td>
+			<td><img src="icons_maps/green.png" width="20" height="20"> / 10</td>
+			<td>13</td>
 			</tr>
 			<tr>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
+				<td>FT</td>
+				<td><img src="icons_maps/red.png" width="20" height="20"> / 4</td>
+				<td><img src="icons_maps/green.png" width="20" height="20"> / 20</td>
+				<td>24</td>
 			</tr>
 			<tr>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
+				<td>PM</td>
+				<td><img src="icons_maps/red.png" width="20" height="20"> / 2</td>
+				<td><img src="icons_maps/green.png" width="20" height="20"> / 7</td>
+				<td>9</td>
 			</tr>
-			<tr>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-			</tr>
-			<tr>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-				<td>item</td>
-			</tr>
+		
 		</tbody>
 	</table>
 	</div>
