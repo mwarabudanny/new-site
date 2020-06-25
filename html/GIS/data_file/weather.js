e5f12ee367abc69a94c01de4cb10df7b63bdc281
mwarabudanny <mwarabudannygmail.com>
2020-06-25 18:56:46 +0200

    DADAD

--- html/GIS/data_file/weather.js
+++ html/GIS/data_file/weather.js
@@ -1,32 +1,32 @@
- var all_cities=[]
-     
-$( document ).ready(function() {
-     //var cities=['lubumbashi','pretoria','johannesburg','cape town','durban'];
-     var cities=['lubumbashi'];
-     
+/* var a = function(callback)
+{
+    
+
+     var cities=['lubumbashi','pretoria','johannesburg','cape town','durban'];
+     //var cities=['lubumbashi'];
+     var all_cities= '';
      for(i=0;i<cities.length;i++){
+        
         var city=cities[i];
         var api_url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=847dd7d4f3281970db62a2928b0be868';
         $.ajax({
-        url: api_url,
-        type: "GET",
-        dataType: 'jsonp', 
-        async: true,
-        success: function(results){
-            console.log(" results: "+JSON.stringify(results))
-            var all_data=results
-            all_cities.push(all_data)
-            
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-     alert("weather Sno----------------------------");
-  }
-        
-  });
+            url: api_url,
+              dataType: 'json',
+            async: false,
+            success: callback
+          }); 
     }
-    
+       
+};
 
-})
 
-console.log(" all_cities: "+all_cities)
+a(function(data) {
+    console.log(data);
+    alert(JSON.stringify(data));
+});
+      
 
+
+
+
+ */
--- routes/device.js
+++ routes/device.js
@@ -14,8 +14,7 @@ const RoleAuth = require('../middleware/roles-auth');
 
             if (results){
               for(i=0;i<results.length;i++){
-                
-              var dataObj={}
+                 var dataObj={}
                 var data=results[i]
                 var device_id= data.device_id;
                 var device_name=data.device_name;
@@ -26,17 +25,23 @@ const RoleAuth = require('../middleware/roles-auth');
                 var longitude=data.longitude;
                 var device_status=data.device_status
                 var record_date=data.record_date
-                
                 var imageUrl=''
                 if(device_status=='0'){
-                  console.log("is one"+device_id)
+                 // console.log("is one"+device_id)
                   imageUrl+='<center><img src="icons_maps/red.png" width="20" height="20"></center>';
                   
                 }else 
                 if(device_status=='1'){
                   imageUrl+='<center><img src="icons_maps/green.png" width="20" height="20"></center>'
                
-                } 
+                }else 
+                if(device_status=='2'){
+                  imageUrl+='<center><img src="icons_maps/orange.png" width="20" height="20"></center>'
+               
+                } else{
+                  imageUrl+='<center><img src="icons_maps/white.png" width="20" height="20"></center>'
+               
+                }
                 dataObj.device_id=device_id
                 dataObj.device_name=device_name
                 dataObj.device_address=device_address
@@ -52,7 +57,7 @@ const RoleAuth = require('../middleware/roles-auth');
 
               }
               
-            console.log('all_dataall_dataall_data'+JSON.stringify(all_data))
+           // console.log('all_dataall_dataall_data'+JSON.stringify(all_data))
               
             }
              var data={"data": all_data}
