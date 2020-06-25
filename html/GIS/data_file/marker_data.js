ba7a83505ed855a836794bd3018ee699a98755ef
mwarabudanny <mwarabudannygmail.com>
2020-06-25 20:36:17 +0200

    kjv;jgfbaf

--- html/GIS/data_file/marker_data.js
+++ html/GIS/data_file/marker_data.js
@@ -12,38 +12,5 @@ var markers = [
  
   }
 ];
-var a = function(callback)
-{
-    
-
-     var cities=['lubumbashi','pretoria','johannesburg','cape town','durban'];
-     //var cities=['lubumbashi'];
-     var all_cities= '';
-     for(i=0;i<cities.length;i++){
-        
-        var city=cities[i];
-        var api_url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=847dd7d4f3281970db62a2928b0be868';
-        $.ajax({
-            url: api_url,
-              dataType: 'json',
-            async: false,
-            success: callback
-          }); 
-    }
-       
-};
-
-
-a(function(data) {
-    //console.log(data);
-    //alert(JSON.stringify(data));
-    markers.push(data)
-});
-      
-
-
-
-
-console.log(markers)
     
    
--- html/GIS/data_file/weather.js
+++ html/GIS/data_file/weather.js
@@ -1,8 +1,9 @@
-/* var a = function(callback)
+
+var a = function(callback)
 {
     
 
-     var cities=['lubumbashi','pretoria','johannesburg','cape town','durban'];
+     var cities=['lubumbashi','pretoria','johannesburg','cape town','durban','maputo','mossel bay'];
      //var cities=['lubumbashi'];
      var all_cities= '';
      for(i=0;i<cities.length;i++){
@@ -11,7 +12,7 @@
         var api_url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=847dd7d4f3281970db62a2928b0be868';
         $.ajax({
             url: api_url,
-              dataType: 'json',
+              dataType: 'jsonp',
             async: false,
             success: callback
           }); 
@@ -21,12 +22,24 @@
 
 
 a(function(data) {
-    console.log(data);
-    alert(JSON.stringify(data));
+    console.log(data)
+   weather_data={};
+   lat_w=data.coord.lat
+   lng_w=data.coord.lon
+   name=data.name
+   icon=data.weather[0].icon
+   status=data.weather[0].main
+   description=data.weather[0].description
+   weather_data.coords={lat: lat_w, lng: lng_w}
+   weather_data.content=`<h5>City: ${name}</h5>
+                         <h5>Status: ${status}</h5>
+                         <h5>Desc: ${description}</h5>`
+   weather_data.iconImage=`http://openweathermap.org/img/wn/${icon}@2x.png`
+    markers.push(weather_data)
 });
       
 
 
 
 
- */
+console.log(markers)
