16095e763fe37894d15819c8851300f33059e4d1
mwarabudanny <mwarabudannygmail.com>
2020-06-25 14:52:25 +0200

    sfsfsf

--- routes/device.js
+++ routes/device.js
@@ -7,7 +7,7 @@ const RoleAuth = require('../middleware/roles-auth');
 
 
  //router.get('/',checkAuth,RoleAuth.admin_role,(req,res)=>{
-  router.get('/',(req,res)=>{
+  router.get('/',checkAuth,(req,res)=>{
           device.find().exec().then( results=>{
             console.log()
             all_data=[]
