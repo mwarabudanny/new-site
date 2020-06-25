82f57804e7e968cbe8ae4f5e70e19fc7c2a1c0ae
mwarabudanny <mwarabudannygmail.com>
2020-06-25 22:00:17 +0200

    ihiohih

--- html/GIS/data_file/marker_data.js
+++ html/GIS/data_file/marker_data.js
@@ -17,21 +17,23 @@ var a = function(callback)
 a(function(results) {
     if (results&&results.data){
 
+      var data_all=results.data
+      for(i=0;i<data_all.length;i++){
+        var data=data_all[i]
+        device_data={};
+        device_lat=data.lattiude*1
+        device_lng=data.longitude*1
+        device_id=data.device_id
+        device_status=data.device_status
+        device_data.coords={lat: device_lat, lng: device_lng}
+        device_data.content=`<h5>Device: ${device_id}</h5>
+                              <h5>Status: ${data.region}</h5>`
+         device_data.iconImage='https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+         
+         console.log('))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))00'+JSON.stringify(device_data))
+         markers.push(device_data) 
+      }
 
-      console.log('))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))00'+JSON.stringify(results.data))
-      /* device_data={};
-   device_lat=data.coord.lat
-   device_lng=data.coord.lon
-   name=data.name
-   icon=data.weather[0].icon
-   status=data.weather[0].main
-   description=data.weather[0].description
-   weather_data.coords={lat: lat_w, lng: lng_w}
-   weather_data.content=`<h5>City: ${name}</h5>
-                         <h5>Status: ${status}</h5>
-                         <h5>Desc: ${description}</h5>`
-   weather_data.iconImage=`http://openweathermap.org/img/wn/${icon}@2x.png`
-    markers.push(weather_data) */
     }
 
 });})
@@ -48,17 +50,7 @@ console.log(markers)
 
 var lat='£'
 var markers = [
-  {
-    coords:{lat:-25.740603,lng:28.209105},
-    iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    content:`<h1>${lat} Lynn MA</h1>`
-  },
- 
-  {
-    coords:{lat:-24.734534603,lng:27.709105},
-    iconImage:'http://openweathermap.org/img/wn/10d@2x.png'
- 
-  }
+  
 ];
     
    
