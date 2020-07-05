b9b143b0c5150f13b180e7746ca19e5f8bff2962
mwarabudanny <mwarabudanny@gmail.com>
2020-07-05 14:03:02 +0200

    hhhhh

--- models/vadalism.js
+++ models/vadalism.js
@@ -8,8 +8,11 @@ const vandalismSchema = mongoose.Schema({
       area:{  type: String, required: false}, 
       latitude: { type: String, required: false },
       longitude:{  type: String, required: false}, 
+      solved_by:{  type: String, required: false},
       transformer_ID:{ type: String},
-      record_date: {type: Date,default: Date.now},
-      image_url:{ type: String},});
+      report_date: {type: Date,default: Date.now},
+      solved_date: {type: Date},
+      image_url:{ type: String}
+});
 
 module.exports = mongoose.model('Vandalism', vandalismSchema);
